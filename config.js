46dcdde92c2f50b553cb060ce523e9d93334dba2
sentircreativo <85263371+sentircreativo@users.noreply.github.com>
2022-05-18 23:31:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbwJWrEk1z0gIifhqF1s3FsWtIbtbEFhmwVpEoN0ubmVmn8y9rwCSNQkyKlvgah3Yb06/exec";
